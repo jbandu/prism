1bfcb999988900d11a2da6b156d90e914a1cc2d9
jbandu <jbandu@gmail.com>
2025-11-04 18:30:04 -0600

    Remove temporary query file

--- prism-web/query_schema.sql
+++
@@ -1,8 +0,0 @@
--- Get all tables with row counts and sizes
-SELECT 
-    schemaname,
-    tablename,
-    pg_size_pretty(pg_total_relation_size(schemaname||'.'||tablename)) AS size
-FROM pg_tables 
-WHERE schemaname NOT IN ('pg_catalog', 'information_schema')
-ORDER BY schemaname, tablename;
